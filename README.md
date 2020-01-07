47acbd9766e9a75745de49f579c1437c48869fe4
WhatsCS <whatscs@jointheb.org>
2020-01-07 15:32:30 -0800

    Forgetful Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# Portfolio
+
+Literally just my website version of a portfolio. I don't know why you'd want to clone this but go for it, I guess.
---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import { LinkContainer } from "react-router-bootstrap";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function Home() {
+  return (
+    <Container id="Home">
+      <Row>
+        <Col>
+          <Image src="/rp.jpg" />
+        </Col>
+        <Col>
+          <Card bg="light">
+            <Card.Img src="/avatar.jpg" />
+            <Card.Body>
+              <Card.Title>Evan Valmassoi</Card.Title>
+              <Card.Subtitle className="text-muted">
+                B.S. Computer Science
+              </Card.Subtitle>
+              <Card.Text>
+                Computer Science Major, Gamer, Linux enthusiast. Since his first
+                computer until now, Evan has been tinkering with technology.
+                Building and trouble shooting desktops, configuring and
+                maintaining servers or just writing software.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+class Projects extends React.Component {
+  getProjects() {}
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar variant="dark" bg="dark">
+          <LinkContainer to="/">
+            <Navbar.Brand className="col-sm-10">Portfolio</Navbar.Brand>
+          </LinkContainer>
+          <Nav className="col-sm-2">
+            <Nav.Item>
+              <LinkContainer to="/Home">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+            <Nav.Item>
+              <LinkContainer to="/Projects">
+                <Nav.Link>Projects</Nav.Link>
+              </LinkContainer>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/Projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
