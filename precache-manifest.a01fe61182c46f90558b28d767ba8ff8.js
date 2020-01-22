ee1663e715ad9f5582e536883962b11d14fa78f0
WhatsCS <whatscs@jointheb.org>
2020-01-21 21:28:27 -0800

    Updates

---
+++ precache-manifest.a01fe61182c46f90558b28d767ba8ff8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322c5cfa08cff90130ade19c172efc65",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ec7ad764beba2ce93811",
+    "url": "/Portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "74a9dbe365b51d49ff47",
+    "url": "/Portfolio/static/css/main.dc8d923b.chunk.css"
+  },
+  {
+    "revision": "ec7ad764beba2ce93811",
+    "url": "/Portfolio/static/js/2.951573e0.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Portfolio/static/js/2.951573e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74a9dbe365b51d49ff47",
+    "url": "/Portfolio/static/js/main.11124473.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  }
+]);
